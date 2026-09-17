511275ffb0d690bae1d13a0b2f2343857656885e
prem-web.dev <a70064182@gmail.com>
2026-09-18 00:50:00 +0530

    Update Microsoft Clarity integration in layout
    Replaced inline script with Script component for Microsoft Clarity.

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,14 +32,25 @@ export default function RootLayout({
         >
              <html lang="en" suppressHydrationWarning>
                     <head>
-                  <script type="text/javascript">
-    (function(c,l,a,r,i,t,y){
-        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", "yjv2tat8jb");
-</script>
-                </head>
+                <head>
+    <Script
+        id="microsoft-clarity"
+        strategy="afterInteractive"
+    >
+        {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){
+                    (c[a].q=c[a].q||[]).push(arguments)
+                };
+                t=l.createElement(r);
+                t.async=1;
+                t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];
+                y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "yjv2tat8jb");
+        `}
+    </Script>
+</head>
                      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`} suppressHydrationWarning>
                         <ConvexClientProvider>{children}</ConvexClientProvider>
                      </body>
